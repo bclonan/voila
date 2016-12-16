a05993cd8cb391c25fa9c8610393e4fa711558e1
Daniel Ostapenko <danielostapenko@gmail.com>
2016-12-16 16:00:15 +0100

    Add Cors to api

--- api/app.py
+++ api/app.py
@@ -2,7 +2,13 @@ import falcon
 
 import converter
 
-api = application = falcon.API()
+class CorsMiddleware(object):
+
+    def process_request(self, request, response):
+        origin = request.get_header('Origin')
+        response.set_header('Access-Control-Allow-Origin', '*')
+
+api = application = falcon.API(middleware=[CorsMiddleware()])
 
 converter = converter.Converter()
 api.add_route('/converter', converter)
--- api/converter.py
+++ api/converter.py
@@ -15,7 +15,6 @@ class Converter():
         formStyle =  'width:100%;height:500px;'
         embedCode = ('<!-- Change the width and height values to suit you best -->'
         '<div class="typeform-widget" data-url="__embed_url__" data-text="__form_name__" style="__style__"></div>'
-        '<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id="typef_orm",b="https://s3-eu-west-1.amazonaws.com/share.typeform.com/";if(!gi.call(d,id)){js=ce.call(d,"script");js.id=id;js.src=b+"widget.js";q=gt.call(d,"script")[0];q.parentNode.insertBefore(js,q)}})()</script>'
         '<div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by<a href="https://www.typeform.com/examples/forms/?utm_campaign=__form_uid__&amp;utm_source=typeform.com-5872961-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-onlineform&amp;utm_term=EN" style="color: #999" target="_blank">Typeform</a></div>')
 
         embedCode = embedCode.replace('__form_uid__', formUid)
