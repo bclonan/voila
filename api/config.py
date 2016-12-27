42280d0cad9427d6dd02ef59aa1c0065a5582e36
Daniel Ostapenko <danielostapenko@gmail.com>
2016-12-27 14:06:13 +0100

    Remove unused files

--- api/config.py
+++
@@ -1,9 +0,0 @@
-# Dev overrides
-
-#TYPEFORM API
-
-TYPEFORM_API_KEY = '2a0dfda4fcb1a6b38dfd7406f501174409c3ad46'
-TYPEFORM_API_CREATE_URL = 'https://admin.typeform.com/forms'
-TYPEFORM_RENDER_BASE_URL = 'https://admin.typeform.com/to/'
-TYPEFORM_DUPLICATE_BASE_URL = 'https://admin.typeform.com/form/template/'
-VERIFY_CREDENTIALS = False
