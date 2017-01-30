d6078878bdfb928737b8fb05c3c5e1958a99823e
Luis R. Palanca <luis@typeform.com>
2017-01-30 18:43:30 +0100

    Move form examples to string

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,32 @@
-import test from 'ava';
-import Tree from './tree'
+import test from 'ava'
+import { elPais, elMundo, restaurant, bank, mailService, airline } from './form.examples'
 
-test('my passing test', t => {
-    var tree = new Tree()
-    t.pass();
-});
+test('Newspaper El País contact form', t => {
+  //console.log(elPais)
+  t.pass()
+})
+
+test('Newspaper El Mundo contact form', t => {
+  //console.log(elMundo)
+  t.pass()
+})
+
+test('Restaurant contact form', t => {
+  //console.log(restaurant)
+  t.pass()
+})
+
+test('Bank Triodos contact form', t => {
+  //console.log(bank)
+  t.pass()
+})
+
+test('Mail service Correos contact form (table)', t => {
+  //console.log(mailService)
+  t.pass()
+})
+
+test('Airline service Ryanair contact form (dt)', t => {
+  //console.log(airline)
+  t.pass()
+})
