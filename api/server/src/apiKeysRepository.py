1c32f9bb2b706a02bc48acaf8ad022e496530a7b
Daniel Ostapenko <danielostapenko@gmail.com>
2017-03-10 13:34:24 +0100

    fix hash keys for  redis

--- api/server/src/apiKeysRepository.py
+++ api/server/src/apiKeysRepository.py
@@ -5,7 +5,7 @@ class ApiKeysRepository():
         self.connection = redis.StrictRedis(host='redis', port=6379, db=0)
 
     def getOriginalApiKey(self, publicKey):
-        return self.connection.get(publicKey)
+        return self.connection.get('user_' + publicKey)
 
     def setOriginalApiKey(self, apiKey, publicKey):
-        return self.connection.set(apiKey, publicKey)
+        return self.connection.set('user_' + apiKey, publicKey)
--- api/server/src/formHashRepository.py
+++ api/server/src/formHashRepository.py
@@ -5,7 +5,7 @@ class FormHashRepository():
         self.connection = redis.StrictRedis(host='redis', port=6379, db=0)
 
     def getFormByHash(self, hash):
-        return self.connection.get(hash)
+        return self.connection.get('form_' + hash)
 
     def setHashForForm(self, hash, form):
-        return self.connection.set(hash, form)
+        return self.connection.set('form_' + hash, form)
